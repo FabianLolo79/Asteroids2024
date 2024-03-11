2b615d589a2532b6b9491aecd68fb2442fdac7b4
Fabián E. García <fabianegl79@gmail.com>
2024-03-11 15:51:55 -0300

    Agregado de texto controles en main menu, Juego finalizado V0.1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
